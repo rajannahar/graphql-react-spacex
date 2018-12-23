598dc388cfcb75160356c1b077d2d41eceed3b9f
R_N101 <R_N101@msn.com>
2018-12-23 20:53:48 +0000

    client

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Launches from './components/Launches';
+import Launch from './components/Launch';
+import './App.css';
+import logo from './logo.png';
+
+const client = new ApolloClient({
+  uri: '/graphql'
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="container">
+            <div className="App">
+              <a href="/">
+                <img src={logo} alt="SpaceX" style={{
+                  width: 300,
+                  display: 'block', 
+                  margin: 'auto'
+                }} />
+              </a>
+
+              {/* <Launches /> */}
+              <Route exact path="/" component={Launches} />
+              <Route exact path="/launch/:flight_number" component={Launch} />
+            </div>
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
