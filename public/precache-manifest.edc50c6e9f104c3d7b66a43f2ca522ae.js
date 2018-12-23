9b367821173c212f77333cc7b7e030d7100bc156
R_N101 <R_N101@msn.com>
2018-12-23 20:49:18 +0000

    Finished app

---
+++ public/precache-manifest.edc50c6e9f104c3d7b66a43f2ca522ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e33fe4bb39a820873f7b",
+    "url": "/static/js/main.e33fe4bb.chunk.js"
+  },
+  {
+    "revision": "74448b3e6e951fad4308",
+    "url": "/static/js/1.74448b3e.chunk.js"
+  },
+  {
+    "revision": "e33fe4bb39a820873f7b",
+    "url": "/static/css/main.480a1ae8.chunk.css"
+  },
+  {
+    "revision": "fb3b67d94c7d828bb688e93fbbac340c",
+    "url": "/index.html"
+  }
+];
